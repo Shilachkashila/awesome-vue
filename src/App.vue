bcc3847dc7fe2cd2fc0c2c22d08c18c68268c14d
Shilachkashila <67004521+Shilachkashila@users.noreply.github.com>
2021-01-06 18:22:03 +0200

    6.01.21 сохранка шаблона перед веткой.

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,14 @@
 </template>
 
 <script>
-export default {
-name: 'app',
-components: {
+  export default {
+    name: 'app',
+    components: {
 
-}
-}
-
+    }
+  }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 </style>
